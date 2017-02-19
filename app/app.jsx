f1215267237a95abaa27cc61968ab8f0d06757c2
Ryan Haas <Ryan Haas>
2017-02-19 12:05:24 -0600

    added timer and countdown

--- app/app.jsx
+++ app/app.jsx
@@ -1,7 +1,9 @@
-var React                                    = require('react');
-var ReactDOM                                 = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main                                     = require('Main');
+const React                                    = require('react');
+const ReactDOM                                 = require('react-dom');
+const {Route, Router, IndexRoute, hashHistory} = require('react-router');
+const Main                                     = require('Main');
+const Timer                                    = require('Timer');
+const Countdown                                = require('Countdown');
 
 
 
@@ -17,7 +19,8 @@ require('style!css!sass!applicationStyles');
 ReactDOM.render(
 	<Router history={hashHistory}>
 		<Route path="/" component={Main}>
-			
+			<IndexRoute component={Timer}/>
+			<Route path="countdown" component={Countdown}/>
 		</Route>
 	</Router>,
 	document.getElementById('app')
---
+++ app/components/Countdown.jsx
@@ -0,0 +1,19 @@
+const React = require('react');
+
+
+var Countdown = React.createClass({
+
+
+	render: function() {
+		return (
+
+			<div>Countdown.jsx</div>
+
+		);
+	}
+
+
+
+});
+
+module.exports = Countdown;
